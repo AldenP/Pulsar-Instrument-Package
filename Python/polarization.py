2d5c5ce332ba398169db0cb608a25334f0b1bfdf
Alden P <aldenpatenaude@gmail.com>
2024-06-29 16:40:06 -0400

    Update to polarization calculation

--- Python/polarization.py
+++ Python/polarization.py
@@ -12,10 +12,10 @@ def findParam_I(d_0, d_90):
         d_90 - the data for the 90deg detector
         Future note: make sure dimensions of arrays are good. 
     """
-    # raw intensity = (d_0)^2 + (d_90)^2
-    #np.square will square element-by-element and remain an array
-    #np.add adds two arrays
-    I_raw = np.add( np.square(d_0), np.square(d_90) )
+    # raw intensity = (d_0) + (d_90)
+    # -> data is power from detector
+    # np.add adds two arrays
+    I_raw = np.add( d_0, d_90 )
     
     # To normalize the data, find the max and divide all elements by it
     I_max = np.amax(I_raw, axis=0)  # axis may need to change, depending on future implemtation
@@ -29,7 +29,7 @@ def findParam_Q(d_0, d_90):
         Future note: make sure dimensions of arrays are good. 
     """
     # Q = (d_0)^2 - (d_90)^2
-    Q_raw = np.subtract( np.square(d_0), np.square(d_90) ) 
+    Q_raw = np.subtract( d_0, d_90 ) 
     # now Q needs to be normalized ... 
     # does this require using the normalized Intensity, or should d_0, d_90 be normalized first? 
 
@@ -40,7 +40,7 @@ def findParam_U(d_45, d_135):
         Future note: make sure dimensions of arrays are good. 
     """
     # U = (d_45)^2 - (d_135)^2
-    U_raw = np.subtract( np.square(d_45), np.square(d_135) )
+    U_raw = np.subtract( d_45, d_135 )
     # again, U must be normalized.
 
 def findPolarizationAngle(Q_norm, U_norm):
